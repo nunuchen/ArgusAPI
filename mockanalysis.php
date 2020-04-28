fb5fad8de58de2fe7559a645a30a3358b11dcaea
IvanChen <ivan_chenhs@himirror.com>
2020-04-28 09:45:24 +0800

    subgrid.html testing

---
+++ mockanalysis.php
@@ -0,0 +1,3 @@
+<?php
+echo $returnValue = "[{\"staName\":\"station1\",\"order\":1,\"retestRate\":0.05,\"failRate\":0.06},{\"staName\":\"station2\",\"order\":2,\"retestRate\":0.051,\"failRate\":0.059},{\"staName\":\"station3\",\"order\":3,\"retestRate\":0.052,\"failRate\":0.058}]"  
+?>
