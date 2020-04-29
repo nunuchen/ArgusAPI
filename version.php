ad7dff0241039d634cb6db9f118c0c9d6b72a472
IvanChen <ivan_chenhs@himirror.com>
2020-04-29 13:02:50 +0800

    1. mockdata return Project JSON with station array. 2. version with JQgrid version

--- version.php
+++ version.php
@@ -1,4 +1,6 @@
 <?php
 echo $returnValue = "Argus UI version = 0.1.0-202004271030<br>";  
-echo $returnValue = "Argus API version = 0.1.0-202004271030";  
+echo $returnValue = "Argus API version = 0.1.0-202004271030"; 
+echo $returnValue = "jqGrid 5.3.X "; 
+
 ?>
