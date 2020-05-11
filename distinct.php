02209ffccc5bc74fa81e31125be9105272b5575c
IvanChen <ivan_chenhs@himirror.com>
2020-05-11 08:44:28 +0800

    dropdown list by ajax call

---
+++ distinct.php
@@ -0,0 +1,3 @@
+<?php
+echo $returnValue ="{\"projectName\" : [ \"projectA019-001\",\"projectA020-001\",\"projectA021-001\",\"projectA021-002\",\"projectA021-003\",\"projectA021-004\",\"projectA021-005\"]}" ;
+?>
