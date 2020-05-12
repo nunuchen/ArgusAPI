6d825e2852455484670688fc491194d016113c2b
IvanChen <ivan_chenhs@himirror.com>
2020-05-12 13:48:24 +0800

    get dropdown list API

--- distinct.php
+++ distinct.php
@@ -1,3 +1,5 @@
 <?php
-echo $returnValue ="{\"projectName\" : [ \"projectA019-001\",\"projectA020-001\",\"projectA021-001\",\"projectA021-002\",\"projectA021-003\",\"projectA021-004\",\"projectA021-005\"]}" ;
+echo $returnValue ="[{\"id\":\"1\",\"name\":\"Project A019-0001\"},{\"id\":\"5\",\"name\":\"Project A019-0002\"},{\"id\":\"6\",\"name\":\"Project A023-0001\"},{\"id\":\"10\",\"name\":\"Project A025-0011\"},{\"id\":\"13\",\"name\":\"Project A025-0061\"}]" ;
 ?>
+
+
